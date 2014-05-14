c7be5466c6bdb7131c8c5cd6043c81c92955db38
Isaac Mailach <isaac.mailach@gmail.com>
2014-05-13 22:23:06 -0400

    Add last tree images
    Add message fade in and out.

--- Time/js/input.js
+++ Time/js/input.js
@@ -5,10 +5,11 @@ var time = {
 };
 var messageContainer = document.getElementById("message-container");
 var addContent = null;
+var counter = 0;
 
-function checkTime(i) {
-    if (i < 10) {
-        i = "0" + i;
+function checkTime(check) {
+    if (check < 10) {
+        check = "0" + check;
     }
     return i;
 }
@@ -21,27 +22,120 @@ function setTime() {
         time.s++;
     }
     document.getElementById("clock").innerHTML = checkTime(time.m) + ":" + checkTime(time.s);
-	for (message in db) {
-		if (db[message].s === time.s && db[message].m === time.m) {
-			
-		}
-	}
-	$('input').focus();
 }
 
 function getValues() {
     var input = {};
 	input.text = document.getElementById('input').value;
-	input.m = time.m;
-	input.s = time.s;
+	input.i = i;
+	input.id = counter;
 	db.push(input);
 	document.getElementById('form').reset();
 	/* addContent = document.createTextNode("<p>" + input.text + "</p>");
 	messageContainer.appendChild(addContent); */
-	$('#message-container').append('<p>' + input.text + '</p>');
+	$('#message-container').append('<p class="message" id="' + input.id + '">' + input.text + '</p>');
+	
+	counter++;
 	return false;
 }
 
 setInterval(function () { setTime(); }, 1000);
 /* box.addEventListener( 'webkitTransitionEnd', 
     function( event ) { alert( "Finished transition!" ); }, false ); */
+var i = 1;
+
+function aniTimer() {
+    if (i == 240) {
+        i = 1;
+    }
+    
+    if (i == 1) {
+        $('#bg1, #tree1').addClass('opac');
+        
+    } else if (i == 12) {
+        $('#tree2').addClass('opac');
+        $('#tree1').removeClass('opac');
+        $('#bg2').removeClass('disp');
+    } else if (i == 24) {
+        $('#tree3').addClass('opac');
+        $('#tree2').removeClass('opac');
+    } else if (i == 36) {
+        $('#tree4').addClass('opac');
+        $('#tree3').removeClass('opac');
+    } else if (i == 48) {
+        $('#tree5').addClass('opac');
+        $('#tree4').removeClass('opac');
+    } else if (i == 60) {
+        $('#bg2, #tree6').addClass('opac');
+        $('#tree5').removeClass('opac');
+    } else if (i == 72) {
+        $('#tree7').addClass('opac');
+        $('#tree6').removeClass('opac');
+    } else if (i == 84) {
+        $('#tree8').addClass('opac');
+        $('#tree7').removeClass('opac');
+    } else if (i == 96) {
+        $('#tree9').addClass('opac');
+        $('#tree8').removeClass('opac');
+    } else if (i == 108) {
+        $('#tree10').addClass('opac');
+        $('#tree9').removeClass('opac');
+    } else if (i == 120) {
+        $('#bg3').addClass('opac');
+        $('#bg1').removeClass('opac');
+        $('#bg1').addClass('disp');
+    } else if (i == 126) {
+        $('#tree9').addClass('opac');
+        $('#tree10').removeClass('opac');
+    } else if (i == 138) {
+        $('#tree8').addClass('opac');
+        $('#tree9').removeClass('opac');
+    } else if (i == 150) {
+        $('#tree7').addClass('opac');
+        $('#tree8').removeClass('opac');
+    } else if (i == 162) {
+        $('#tree6').addClass('opac');
+        $('#tree7').removeClass('opac');
+    } else if (i == 174) {
+        $('#tree5').addClass('opac');
+        $('#tree6').removeClass('opac');
+    } else if (i == 180) {
+        $('#bg1').removeClass('disp');
+        $('#bg3').removeClass('opac');
+        $('#bg2').removeClass('opac');
+        $('#bg2').addClass('disp');
+    } else if (i == 186) {
+        $('#tree4').addClass('opac');
+        $('#tree5').removeClass('opac');
+    } else if (i == 198) {
+        $('#tree3').addClass('opac');
+        $('#tree4').removeClass('opac');
+    } else if (i == 210) {
+        $('#tree2').addClass('opac');
+        $('#tree3').removeClass('opac');
+    } else if (i == 222) {
+        $('#tree1').addClass('opac');
+        $('#tree2').removeClass('opac');
+    } else if (i == 234) {
+        $('#tree1').removeClass('opac');
+    }
+    
+    for (message in db) {
+		if (db[message].i === i) {
+            //$('#' + db[message].id).css("height", $('#' + db[message].id).height());
+			$('#' + db[message].id).addClass("opac");
+           // $('#' + db[message].id).slideDown(500);
+		} else if (db[message].i + 7 === i) {
+			$('#' + db[message].id).removeClass("opac");
+            //$('#' + db[message].id).css({'top': '-30px'})
+        } else if (db[message].i -1 === i) {
+            $('#' + db[message].id).removeClass("disp");
+        } else if (db[message].i + 9 === i) {
+            $('#' + db[message].id).addClass("disp");
+        }
+	}
+	$('input').focus();
+    i++;
+}
+
+setInterval(function() {aniTimer();},1000);
